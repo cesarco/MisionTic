64cef89f41c8a9e507242eaf2156f2157cfbc0b0
Cesar Roriguez <rodriguezcesar_125@outlook.es>
2021-08-13 09:05:07 -0500

    Ejercicio de refuerzo

---
+++ s15/15.1_ejercicio_refuerzo.py
@@ -0,0 +1,18 @@
+
+"""implementa un programa que muestre todos los multiplos de 6 entre 6 y 150, ambos inclusive"""
+
+""" numero = int(input("Digite un numero entero: "))
+
+while numero <= 0:
+    numero = int(input("ERRROR -> Digite un numero entero: "))
+
+print(F"Se ingresp correctamente el numero {numero}")
+
+if numero % 6 == 0: """
+
+for numeros in range (6,151,6):
+    print(numeros)
+
+
+
+
---
+++ s15/15.2_ejercicio_refuerzo.py
@@ -0,0 +1,6 @@
+numero_1 = int(input("Ingrese un numero n: "))
+numero_2 = int(input("Ingrese un numero m: "))
+
+
+for numeros in range (numero_1,numero_1,numero_2+1):
+    print(numeros)
