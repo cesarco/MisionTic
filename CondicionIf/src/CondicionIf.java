4594f0f2e7e175213d36670482976ca93e006523
Cesar Roriguez <devcesarco@outlook.es>
2021-08-25 10:13:04 -0500

    Condicional if anidada 2 notas

--- CondicionIf/src/CondicionIf.java
+++ CondicionIf/src/CondicionIf.java
@@ -14,25 +14,42 @@ public class CondicionIf {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce el precio del producto :");
+//        System.out.println("Introduce el precio del producto :");
+//
+//        double precio = sc.nextDouble();
+//        System.out.println("Introduce el numero de unidades: ");
+//
+//        int unidades = sc.nextInt();
+//        double precioCompra = precio * unidades;
+//        double descuento;
+//
+//        if (precioCompra < 100) {
+//            descuento = 0;
+//        } else if (precioCompra > 200) {
+//            descuento = (15 * precioCompra) / 100;
+//
+//        } else {
+//            descuento = (10 * precioCompra) / 100;
+//        }
+//        double precioFinal = precioCompra - descuento;
+//        System.out.println("Precio final " + precioFinal);
+        System.out.println("Introduce la nota entre 0 y 10 : ");
+        float nota = sc.nextFloat();
 
-        double precio = sc.nextDouble();
-        System.out.println("Introduce el numero de unidades: ");
-
-        int unidades = sc.nextInt();
-        double precioCompra = precio * unidades;
-        double descuento;
-
-        if (precioCompra < 100) {
-            descuento = 0;
-        } else if (precioCompra > 200) {
-            descuento = (15 * precioCompra) / 100;
-
-        } else {
-            descuento = (10 * precioCompra) / 100;
+        if (nota >= 0 && nota < 3) {
+            System.out.println("Deficiente..");
+        } else if (nota >= 3 && nota < 5) {
+            System.out.println("Insuficiente..");
+        } else if (nota >= 5 && nota < 6) {
+            System.out.println("Suficiente..");
+        } else if (nota >= 6 && nota < 7) {
+            System.out.println("Bien..");
+        } else if (nota >= 7 && nota < 9) {
+            System.out.println("Excelente..");
+        } else if (nota >= 10 && nota <= 10) {
+            System.out.println("Sobresaliente..");
+        }else {
+            System.out.println("Nota no valida");
         }
-        double precioFinal = precioCompra - descuento;
-        System.out.println("Precio final " + precioFinal);
-
     }
 }
